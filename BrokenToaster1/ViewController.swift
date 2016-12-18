143e387624cf41b7981be660ae17b1d3211e5b7b
Emily Kolar <emkolar@gmail.com>
2016-12-18 16:16:05 -0600

    Initial Commit

---
+++ BrokenToaster1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrokenToaster1
+//
+//  Created by Emily Kolar on 12/18/16.
+//  Copyright © 2016 Emily Kolar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
